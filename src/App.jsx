158240258f1774c888872a7070cbf2bedf1d8c17
AhsanYousaf <ahsanbhatti624@gmail.com>
2024-01-13 19:54:34 +0500

    set up app and installed tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <Navbar />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div>Books</div>
+          <div>
+            <ul>
+              <li>Home</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
